218fc6f557697042317287d21d259e92162d60d0
sindhu mallipeddy <sindhumallipeddy@gmail.com>
2018-04-09 23:57:04 +0000

    added contacts route

--- db/migrate/20180409232514_create_contacts.rb
+++ db/migrate/20180409232514_create_contacts.rb
@@ -1,9 +1,9 @@
 class CreateContacts < ActiveRecord::Migration
   def change
-    create_table :contacts do |t|
+      create_table :contacts do |t|
       t.string :name
       t.string :email
-      t.string :comments
+      t.text :comments
       
       t.timestamps
     end
